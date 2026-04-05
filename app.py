4b386ce5e000aa6f5bc1dfbf3852d067d08164ed
Atsaalnajwan7 <atsalnajwan@gmail.com>
2026-04-05 11:25:13 +0700

    fix predict

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 
-# --- 1. DATA DUMMY (Contoh Data Historis UMKM) ---
-# Kita buat data sederhana agar model bisa belajar pola pertumbuhannya
-# X = Tahun, y = Jumlah UMKM
+# --- DATA DUMMY ---
 X = np.array([[2020], [2021], [2022], [2023], [2024]])
 y = np.array([1000, 1100, 1250, 1320, 1450])
 
-# --- 2. TRAINING MODEL ---
+# --- TRAIN MODEL ---
 model = LinearRegression()
 model.fit(X, y)
 
@@ -20,26 +18,23 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Ambil data tahun dari form (input user)
     tahun_input = request.form.get('tahun')
     
     if tahun_input:
         try:
-            # Ubah input menjadi angka (float)
             tahun = float(tahun_input)
-            
-            # Lakukan prediksi menggunakan model yang sudah di-fit
-            # Kita bungkus dalam [[ ]] karena model minta format 2D array
             prediksi = model.predict([[tahun]])
-            
-            # Ambil hasil prediksi dan bulatkan agar rapi
             hasil = round(prediksi[0], 2)
-            
-            return render_template('index.html', prediction_text=f"{hasil}")
-        except Exception as e:
-            return render_template('index.html', prediction_text="Error dalam perhitungan")
-            
+
+            return render_template(
+                'index.html',
+                prediction_text=f"Perkiraan jumlah UMKM pada tahun {int(tahun)} adalah {hasil}"
+            )
+        except:
+            return render_template('index.html', prediction_text="Terjadi kesalahan input!")
+
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
